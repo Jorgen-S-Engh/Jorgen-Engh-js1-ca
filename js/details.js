b2d78999f5673511c15bf4e8a5154b1a10167a6d
Jorgen-S-Engh <55982088+Jorgen-S-Engh@users.noreply.github.com>
2022-02-21 19:39:47 +0100

    made index and details

--- js/details.js
+++ js/details.js
@@ -1,44 +1,38 @@
 // const container = document.querySelector(".container")
+const city = document.querySelector(".city-details");
+const loader = document.querySelector(".loader-container");
 
 const queryString = document.location.search;
-const params = new URLSearchParams(queryString)
+const params = new URLSearchParams(queryString);
 
-const id = params.get("city")
-console.log(params)
-console.log(id)
+const id = params.get("id");
+console.log(params);
+console.log(id);
 
-
-async function getApi(){
-
-  
-
-  const url = "https://wft-geo-db.p.rapidapi.com/v1/geo/cities/123214" 
+async function getApi() {
+  const url = "https://wft-geo-db.p.rapidapi.com/v1/geo/cities/" + id;
 
   const response = await fetch(url, {
-    "method": "GET",
-    "headers": {
+    method: "GET",
+    headers: {
       "x-rapidapi-host": "wft-geo-db.p.rapidapi.com",
-      "x-rapidapi-key": "7a735c65b0msh4b4917a539d36dap135f69jsna87e32d3342f"
-    }
-  })
-
+      "x-rapidapi-key": "7a735c65b0msh4b4917a539d36dap135f69jsna87e32d3342f",
+    },
+  });
 
   const json = await response.json();
   const data = json.data;
-  console.log(data.id)
+  console.log(data.id);
+  loader.innerHTML = "";
+  city.innerHTML = `
+  <div>
+    <h1>${data.city}</h1>
+    <h3>Country: ${data.country}</h3>
+    <h3>Region: ${data.region}</h3>
+    
 
-  for (let i = 0; i < data.length; i++){
-
-
-
-  }
-
-  console.log(data);
-
-
-
-  
-
+    
+  </div>`;
 }
 
 getApi();
--- js/script.js
+++ js/script.js
@@ -1,33 +1,33 @@
-const container = document.querySelector(".container")
+const container = document.querySelector(".container");
+const loader = document.querySelector(".loader-container");
+const loaderCon = document.querySelector(".loader-container");
 
-
-async function getApi(){
-  const response = await fetch("https://wft-geo-db.p.rapidapi.com/v1/geo/cities", {
-    "method": "GET",
-    "headers": {
-      "x-rapidapi-host": "wft-geo-db.p.rapidapi.com",
-      "x-rapidapi-key": "7a735c65b0msh4b4917a539d36dap135f69jsna87e32d3342f"
+async function getApi() {
+  const response = await fetch(
+    "https://wft-geo-db.p.rapidapi.com/v1/geo/cities",
+    {
+      method: "GET",
+      headers: {
+        "x-rapidapi-host": "wft-geo-db.p.rapidapi.com",
+        "x-rapidapi-key": "7a735c65b0msh4b4917a539d36dap135f69jsna87e32d3342f",
+      },
     }
-  })
+  );
   const json = await response.json();
   const data = json.data;
 
-  for (let i = 0; i < data.length; i++){
-    console.log(data[i].city)
-    console.log(data[i].country)
-    console.log(data[i].region)
+  loader.innerHTML = "";
+  loaderCon.style.width = "0px";
 
-    container.innerHTML += `<a href="details.html?id=${data[i].id}">${data[i].city}</a>`
-    
+  for (let i = 0; i < data.length; i++) {
+    console.log(data[i].city);
+    console.log(data[i].country);
+    console.log(data[i].region);
 
+    container.innerHTML += `<a href="details.html?id=${data[i].id}"><div class="city-item"><h3>${data[i].city}</h3></div></a>`;
   }
 
   console.log(data);
-
-
-
-  
-
 }
 
 getApi();
