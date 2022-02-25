0f93da7473c96bedcd8438f284ffed9f2ca72669
Jorgen-S-Engh <55982088+Jorgen-S-Engh@users.noreply.github.com>
2022-02-25 20:40:24 +0100

    made try/catch

--- js/details.js
+++ js/details.js
@@ -6,30 +6,35 @@ const queryString = document.location.search;
 const params = new URLSearchParams(queryString);
 const id = params.get("id");
 
-
 async function getApi() {
-  const url = "https://wft-geo-db.p.rapidapi.com/v1/geo/cities/" + id;
+  try {
+    const url = "https://wft-geo-db.p.rapidapi.com/v1/geo/cities/" + id;
 
-  const response = await fetch(url, {
-    method: "GET",
-    headers: {
-      "x-rapidapi-host": "wft-geo-db.p.rapidapi.com",
-      "x-rapidapi-key": "7a735c65b0msh4b4917a539d36dap135f69jsna87e32d3342f",
-    },
-  });
+    const response = await fetch(url, {
+      method: "GET",
+      headers: {
+        "x-rapidapi-host": "wft-geo-db.p.rapidapi.com",
+        "x-rapidapi-key": "7a735c65b0msh4b4917a539d36dap135f69jsna87e32d3342f",
+      },
+    });
 
-  const json = await response.json();
-  const data = json.data;
-  console.log(data.id);
-  loader.innerHTML = "";
-  city.innerHTML = `
-    
-    <h1>City Name: ${data.city}</h1>
-    <h4>Country: ${data.country}</h4>
-    <h4>Region: ${data.region}</h4>
-    <h4>Elevation Meters: ${data.elevationMeters}</h4>
-    <h4>Latitude: ${data.latitude}</h4>
-    <h4>Longitude: ${data.longitude}</h4>`;
+    const json = await response.json();
+    const data = json.data;
+    console.log(data.id);
+    loader.innerHTML = "";
+    city.innerHTML = `
+    <div class="city-container">
+      <h1>City Name: ${data.city}</h1>
+      <h4>Country: ${data.country}</h4>
+      <h4>Region: ${data.region}</h4>
+      <h4>Elevation Meters: ${data.elevationMeters}</h4>
+      <h4>Latitude: ${data.latitude}</h4>
+      <h4>Longitude: ${data.longitude}</h4>
+    </div>`;
+  } catch (err) {
+    loader.innerHTML = "";
+    city.innerHTML = `<div class="api-error-div"><h2>OH NO! Something went wrong, no flight for you</h2><img src="icon/noun-no-flight-3339085.png" alt=""></img></div>`;
+  }
 }
 
 getApi();
--- js/script.js
+++ js/script.js
@@ -3,36 +3,40 @@ const loader = document.querySelector(".loader-container");
 const loaderCon = document.querySelector(".loader-container");
 
 async function getApi() {
-  const response = await fetch(
-    "https://wft-geo-db.p.rapidapi.com/v1/geo/cities",
-    {
-      method: "GET",
-      headers: {
-        "x-rapidapi-host": "wft-geo-db.p.rapidapi.com",
-        "x-rapidapi-key": "7a735c65b0msh4b4917a539d36dap135f69jsna87e32d3342f",
-      },
+  try {
+    const response = await fetch(
+      "https://wft-geo-db.p.rapidapi.com/v1/geo/cities",
+      {
+        method: "GET",
+        headers: {
+          "x-rapidapi-host": "wft-geo-db.p.rapidapi.com",
+          "x-rapidapi-key":
+            "7a735c65b0msh4b4917a539d36dap135f69jsna87e32d3342f",
+        },
+      }
+    );
+    const json = await response.json();
+    const data = json.data;
+
+    loader.innerHTML = "";
+    loaderCon.style.width = "0px";
+
+    for (let i = 0; i < data.length; i++) {
+      console.log(data[i].city);
+      console.log(data[i].country);
+      console.log(data[i].region);
+
+      container.innerHTML += `
+      <a href="details.html?id=${data[i].id}">
+        <div class="city-item">
+          <h3>${data[i].city}</h3>
+        </div>
+      </a>`;
     }
-  );
-  const json = await response.json();
-  const data = json.data;
-
-  loader.innerHTML = "";
-  loaderCon.style.width = "0px";
-
-  for (let i = 0; i < data.length; i++) {
-    console.log(data[i].city);
-    console.log(data[i].country);
-    console.log(data[i].region);
-
-    container.innerHTML += `
-    <a href="details.html?id=${data[i].id}">
-      <div class="city-item">
-        <h3>${data[i].city}</h3>
-      </div>
-    </a>`;
+  } catch (err) {
+    loader.innerHTML = "";
+    container.innerHTML = `<div class="api-error-div"><h2>OH NO! Something went wrong, no flight for you</h2><img src="icon/noun-no-flight-3339085.png" alt=""></img></div>`;
   }
-
-  console.log(data);
 }
 
 getApi();
